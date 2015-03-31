a12c3c80447205b0ce3b0be4a48e47afcefd4e82
tazro inutano ohta <inutano@gmail.com>
2015-03-31 20:05:23 +0900

    prepare for introducing activerecord

--- Gemfile
+++ Gemfile
@@ -2,6 +2,16 @@ source 'https://rubygems.org'
 ruby '2.1.2'
 
 gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'rake'
+gem 'pg'
+
 gem 'haml'
 gem 'sass'
 gem 'redcarpet'
+gem 'rack-protection'
+
+group :development do
+  gem 'sqlite3'
+end
+
--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 # :)
 
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'haml'
 require 'sass'
+require './lib/peakjohn'
+
+ENV["DATABASE_URL"] ||= "sqlite3:database.sqlite"
 
 class PeakJohn < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :database, ENV["DATABASE_URL"]
+
   helpers do
     def app_root
       "#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{env["SCRIPT_NAME"]}"
