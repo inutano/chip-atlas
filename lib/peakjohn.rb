ba33a69bd89b5223434eb334e6c77d47661e5595
tazro inutano ohta <inutano@gmail.com>
2015-04-16 22:36:42 +0900

    refactored existing methods and added filesearch

--- lib/peakjohn.rb
+++ lib/peakjohn.rb
@@ -11,12 +11,32 @@ class Bedfile < ActiveRecord::Base
       self.all.map{|r| r.genome }.uniq
     end
     
+    def index_all_genome
+      index = Hash.new
+      list_of_genome.each do |genome|
+        index[genome] = index_by_genome(genome)
+      end
+      index
+    end
+    
+    def index_by_genome(genome)
+      records = records_by_genome(genome)
+      index_all_facets(records)
+    end
+
     def records_by_genome(genome)
       self.where(:genome => genome)
     end
+
+    def index_all_facets(records)
+      index = Hash.new
+      list_of_facets.each do |facet|
+        index[facet] = index_with_experiment_count(records, facet)
+      end
+      index
+    end
     
-    def index_with_experiment_count(genome, column)
-      records = records_by_genome(genome)
+    def index_with_experiment_count(records, column)
       index = Hash.new(0)
       records.each do |r|
         c_value = r.send(column)
@@ -30,21 +50,21 @@ class Bedfile < ActiveRecord::Base
       end
       index
     end
-
-    def index_all_facets(genome)
-      index = Hash.new
-      list_of_facets.each do |facet|
-        index[facet] = index_with_experiment_count(genome, facet)
-      end
-      index
-    end
     
-    def index_all_genome
-      index = Hash.new
-      list_of_genome.each do |genome|
-        index[genome] = index_all_facets(genome)
-      end
-      index
+    def get_filename(condition)
+      results = filesearch(condition)
+      raise NameError if results.size > 1
+      results.first.filename
+    end
+    # http://localhost:60151/load?file=http://dbarchive.biosciencedbc.jp/kyushu-u/hg19/assembled/#{fname}&genome=hg19
+    
+    def filesearch(condition)
+      self.where(:genome => condition["genome"])
+        .where(:agClass => condition["agClass"])
+        .where(:agSubClass => condition["agSubClass"])
+        .where(:clClass => condition["clClass"])
+        .where(:clSubClass => condition["clSubClass"])
+        .where(:qval => condition["qval"])
     end
   end
 end
