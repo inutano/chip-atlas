71687bcc0c901dfc93055fc2a844badc6bd634ed
Tazro Inutano Ohta <inutano@gmail.com>
2016-02-18 15:01:49 +0900

    protein -> proteins

--- public/js/pj/in_silico_chip.js
+++ public/js/pj/in_silico_chip.js
@@ -567,9 +567,9 @@ var helpText = {
   comparedwithbed: 'Check this to compare \'My data\' with another dataset (UCSC BED format or a sequence motif).\n\n',
   comparedwithrefseq: 'Check this to compare \'My data\' with RefSeq coding genes, excluding those listed in \'My data\'.',
   comparedwithuserlist: 'Check this to compare \'My data\' with another gene list.\n\n',
-  tss: 'To search for protein binding to given genes, specify the distance range from the Transcription Start Sites (TSS).\n\Default is between -5000 and +5000 bp from the TSS.',
+  tss: 'To search for proteins binding to given genes, specify the distance range from the Transcription Start Sites (TSS).\n\Default is between -5000 and +5000 bp from the TSS.',
   userdatadesc: 'Enter a title for the data selected in "4. Select your data".\nAcceptable letters are alphanumeric (a-Z, 0-9), space ( ), underscore (_), period (.) and hyphen (-).',
   comparedwithdesc: 'Enter a title for the data selected in "5. Select data to be compared".\nAcceptable letters are alphanumeric (a-Z, 0-9), space ( ), underscore (_), period (.) and hyphen (-).',
   projectdesc: 'Enter a title for this submission.\nAcceptable letters are alphanumeric (a-Z, 0-9), space ( ), underscore (_), period (.) and hyphen (-).',
-  disttss: 'To search for protein binding to given genes, specify the distance range from the Transcription Start Sites (TSS).\n\Default is between -5000 and +5000 bp from the TSS.'
+  disttss: 'To search for proteins binding to given genes, specify the distance range from the Transcription Start Sites (TSS).\n\Default is between -5000 and +5000 bp from the TSS.'
 };
