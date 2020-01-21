3a9a242208b07cac1e340945440235680a01c145
Tazro Inutano Ohta <inutano@gmail.com>
2020-01-21 15:30:47 +0900

    follow the changes of scripts

--- rdf/generate-rdf.sh
+++ rdf/generate-rdf.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -eux
+mkdir -p "./data/bed"
+mkdir -p "./data/ttl"
 genome_references=(
-  "hg19\tGRCh37"
-  "mm9\tmm9"
-  "rn6\trn6"
-  "dm3\tdm3"
-  "ce10\tce10"
-  "sacCer3\tsacCer3"
+  "hg19"
+  "mm9"
+  "rn6"
+  "dm3"
+  "ce10"
+  "sacCer3"
 )
 threshold=(
   "05"
@@ -16,20 +18,16 @@ threshold=(
 )
 for genome in ${genome_references[@]}; do
   for th in ${threshold[@]}; do
-    sp=$(echo -e "${genome}" | cut -f 1)
-
-    genome_version=$(echo -e "${genome}" | cut -f 2)
     data_version=$(date "+%Y%m%d-%H%M")
-    ttl_fname="chip-atlas.${genome_version}.${data_version}.${th}.ttl"
+    ttl_fname="ChIPAtlas-TFBS-${data_version}.${genome}.${th}.ttl"
 
-    url_base="http://dbarchive.biosciencedbc.jp/kyushu-u/${sp}/allPeaks_light"
-    fname="allPeaks_light.${sp}.${th}.bed.gz"
+    url_base="http://dbarchive.biosciencedbc.jp/kyushu-u/${genome}/allPeaks_light"
+    fname="allPeaks_light.${genome}.${th}.bed.gz"
 
-    wget "${url_base}/${fname}"
-    gunzip -c "${fname}" |\
-     ./bed2ttl \
-       -v data_version=${data_version} \
-       -v genome_version=${genome_version} \
-       > ${ttl_fname}
+    wget -nc -P "./data/bed" "${url_base}/${fname}"
+    gunzip -c "./data/bed/${fname}" |\
+      gawk -f ./bin/chr2genbank -v ref=$(pwd -P)/reference -v genome_version=${genome} |\
+      gawk -f ./bin/bed2ttl -v data_version=${data_version} \
+      > "./data/ttl/${ttl_fname}"
   done
 done
