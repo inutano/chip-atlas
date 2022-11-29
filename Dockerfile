0bcca4083703bb1d8d392f095a00c4efd08c22ef
Tazro Inutano Ohta <inutano@gmail.com>
2022-11-29 14:13:00 +0900

    update schema

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Docker container for chip-atlas dev env
 FROM ruby:3.1.3
-RUN apt-get update -y && apt-get install -y libffi-dev build-essential libpq-dev libsqlite3-dev
+RUN apt-get update -y && apt-get install -y libffi-dev build-essential libpq-dev libsqlite3-dev lftp
 COPY . /app
 WORKDIR /app
 RUN bundle install
--- db/schema.rb
+++ db/schema.rb
@@ -1,29 +1,27 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128015049) do
-
+ActiveRecord::Schema[7.0].define(version: 2015_11_28_015049) do
   create_table "analyses", force: :cascade do |t|
-    t.string  "antigen"
-    t.string  "cell_list"
+    t.string "antigen"
+    t.string "cell_list"
     t.boolean "target_genes"
-    t.string  "genome"
+    t.string "genome"
+    t.datetime "timestamp", precision: nil
+    t.index ["antigen"], name: "index_analyses_on_antigen"
+    t.index ["cell_list"], name: "index_analyses_on_cell_list"
+    t.index ["genome"], name: "index_analyses_on_genome"
+    t.index ["target_genes"], name: "index_analyses_on_target_genes"
   end
-
-  add_index "analyses", ["antigen"], name: "index_analyses_on_antigen"
-  add_index "analyses", ["cell_list"], name: "index_analyses_on_cell_list"
-  add_index "analyses", ["genome"], name: "index_analyses_on_genome"
-  add_index "analyses", ["target_genes"], name: "index_analyses_on_target_genes"
 
   create_table "bedfiles", force: :cascade do |t|
     t.string "filename"
@@ -34,28 +32,28 @@ ActiveRecord::Schema.define(version: 20151128015049) do
     t.string "clSubClass"
     t.string "qval"
     t.string "experiments"
+    t.datetime "timestamp", precision: nil
+    t.index ["agClass"], name: "index_bedfiles_on_agClass"
+    t.index ["agSubClass"], name: "index_bedfiles_on_agSubClass"
+    t.index ["clClass"], name: "index_bedfiles_on_clClass"
+    t.index ["clSubClass"], name: "index_bedfiles_on_clSubClass"
+    t.index ["genome"], name: "index_bedfiles_on_genome"
+    t.index ["qval"], name: "index_bedfiles_on_qval"
   end
-
-  add_index "bedfiles", ["agClass"], name: "index_bedfiles_on_agClass"
-  add_index "bedfiles", ["agSubClass"], name: "index_bedfiles_on_agSubClass"
-  add_index "bedfiles", ["clClass"], name: "index_bedfiles_on_clClass"
-  add_index "bedfiles", ["clSubClass"], name: "index_bedfiles_on_clSubClass"
-  add_index "bedfiles", ["genome"], name: "index_bedfiles_on_genome"
-  add_index "bedfiles", ["qval"], name: "index_bedfiles_on_qval"
 
   create_table "bedsizes", force: :cascade do |t|
-    t.string  "genome"
-    t.string  "agClass"
-    t.string  "clClass"
-    t.string  "qval"
+    t.string "genome"
+    t.string "agClass"
+    t.string "clClass"
+    t.string "qval"
     t.integer "number_of_lines"
+    t.datetime "timestamp", precision: nil
+    t.index ["agClass"], name: "index_bedsizes_on_agClass"
+    t.index ["clClass"], name: "index_bedsizes_on_clClass"
+    t.index ["genome"], name: "index_bedsizes_on_genome"
+    t.index ["number_of_lines"], name: "index_bedsizes_on_number_of_lines"
+    t.index ["qval"], name: "index_bedsizes_on_qval"
   end
-
-  add_index "bedsizes", ["agClass"], name: "index_bedsizes_on_agClass"
-  add_index "bedsizes", ["clClass"], name: "index_bedsizes_on_clClass"
-  add_index "bedsizes", ["genome"], name: "index_bedsizes_on_genome"
-  add_index "bedsizes", ["number_of_lines"], name: "index_bedsizes_on_number_of_lines"
-  add_index "bedsizes", ["qval"], name: "index_bedsizes_on_qval"
 
   create_table "experiments", force: :cascade do |t|
     t.string "expid"
@@ -66,22 +64,22 @@ ActiveRecord::Schema.define(version: 20151128015049) do
     t.string "clSubClass"
     t.string "title"
     t.string "additional_attributes"
+    t.datetime "timestamp", precision: nil
     t.string "clSubClassInfo"
     t.string "readInfo"
+    t.index ["agClass"], name: "index_experiments_on_agClass"
+    t.index ["agSubClass"], name: "index_experiments_on_agSubClass"
+    t.index ["clClass"], name: "index_experiments_on_clClass"
+    t.index ["clSubClass"], name: "index_experiments_on_clSubClass"
+    t.index ["expid"], name: "index_experiments_on_expid"
   end
-
-  add_index "experiments", ["agClass"], name: "index_experiments_on_agClass"
-  add_index "experiments", ["agSubClass"], name: "index_experiments_on_agSubClass"
-  add_index "experiments", ["clClass"], name: "index_experiments_on_clClass"
-  add_index "experiments", ["clSubClass"], name: "index_experiments_on_clSubClass"
-  add_index "experiments", ["expid"], name: "index_experiments_on_expid"
 
   create_table "runs", force: :cascade do |t|
     t.string "runid"
     t.string "expid"
+    t.datetime "timestamp", precision: nil
+    t.index ["expid"], name: "index_runs_on_expid"
+    t.index ["runid"], name: "index_runs_on_runid"
   end
-
-  add_index "runs", ["expid"], name: "index_runs_on_expid"
-  add_index "runs", ["runid"], name: "index_runs_on_runid"
 
 end
