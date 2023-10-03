be1225b92ab3b5281260b9e14773311ca2e0748b
Tazro Inutano Ohta <inutano@gmail.com>
2023-10-03 14:56:07 +0900

    fix bisulfite IGV integration

--- public/js/pj/experiment.js
+++ public/js/pj/experiment.js
@@ -3,7 +3,6 @@ $(function(){
   dbLinkOut();
   analysisLinkOut();
   showHelp();
-  //browseIgv();
   loadImages();
   hideAnalysis();
 })
@@ -88,75 +87,6 @@ function analysisLinkOut(){
       });
     });
   });
-}
-
-function browseIgv(){
-  var expid = getUrlParameter('id');
-  // retrieve metadata
-  $.ajax({
-    type: 'GET',
-    url: '/data/exp_metadata.json?expid=' + expid,
-    dataType: 'json'
-  }).done(function(json){
-    var metadata = json;
-    openIgvUri(expid, metadata);
-  })
-}
-
-function openIgvUri(expid, metadata){
-  var params = getUrlParameters(expid, metadata);
-  $("a.link-igv").on('click', function(){
-    var link = $(this);
-    // check if igv is running
-    var url = getLinkOutUrl(link, expid, metadata);
-    window.open(url, "_self", "")
-    // var igvUrl = 'http://127.0.0.1:60151';
-    // $.ajax({
-    //   type: 'GET',
-    //   url: igvUrl
-    // }).done(function(response){
-    //   var url = getLinkOutUrl(link, expid, metadata);
-    //   window.open(url, "_self", "")
-    // }).fail(function(response){
-    //     alert("IGV is not running on your computer.\n\nLaunch IGV and allow an access via port 60151 (View > Preferences... > Advanced > check 'enable port' and set port number 60151) to browse data.\n\n If you have not installed IGV, visit  https://www.broadinstitute.org/igv/download or google 'Integrative Genomics Viewer' to download the software.");
-    // })
-  });
-}
-
-function getLinkOutUrl(link, expid, metadata){
-  var dType = link.attr("name");
-  var params = getUrlParameters(expid, metadata);
-  var genome = params['genome']
-  var url;
-  switch(dType){
-    case "bigwig":
-      url = params['baseUrl']+'/bw/'+expid+'.bw&genome='+genome+'&name='+params['fname']
-      break;
-    case "bed":
-      var dValue = link.attr("value");
-      url = params['baseUrl']+'/bb'+dValue+'/'+expid+'.'+dValue+'.bb&genome='+genome+'&name='+params['fname']+'%20(1E-'+dValue+')';
-      break;
-  }
-  return url;
-}
-
-function getUrlParameters(expid, metadata){
-  var genome = metadata['genome'];
-  var antigen = metadata["agSubClass"];
-  var celltype = metadata["clSubClass"];
-  var antigenEnc = encodeURI(antigen);
-  var celltypeEnc = encodeURI(celltype);
-  var igvUrl = 'http://localhost:60151/load?file=';
-  var nbdcUrl = 'https://chip-atlas.dbcls.jp/data';
-  var params = {
-    baseUrl: igvUrl+nbdcUrl+"/"+genome+'/eachData',
-    genome: genome,
-    antigen: antigenEnc,
-    celltype: celltypeEnc,
-    fname: encodeURI(antigen+' (@ '+celltype+') '+expid),
-    dfname: genome+'_'+antigenEnc+'_'+celltypeEnc+'_'+expid
-  }
-  return params
 }
 
 // Loading fastqc images
