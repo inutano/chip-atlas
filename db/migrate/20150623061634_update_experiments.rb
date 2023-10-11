13488ae19873dd306ca7f8777c002ba4a98d19da
Tazro Inutano Ohta <inutano@gmail.com>
2023-10-11 15:21:19 +0900

    able to update with docker compose

--- db/migrate/20150623061634_update_experiments.rb
+++ db/migrate/20150623061634_update_experiments.rb
@@ -1,4 +1,4 @@
-class UpdateExperiments < ActiveRecord::Migration
+class UpdateExperiments < ActiveRecord::Migration[4.2]
   def up
     add_column :experiments, :clSubClassInfo, :string
     add_column :experiments, :readInfo, :string
--- db/migrate/20151127142537_create_bedsizes.rb
+++ db/migrate/20151127142537_create_bedsizes.rb
@@ -1,12 +1,12 @@
-class CreateBedsizes < ActiveRecord::Migration
+class CreateBedsizes < ActiveRecord::Migration[4.2]
   def change
     create_table(:bedsizes) do |t|
       t.string :genome
       t.string :agClass
       t.string :clClass
       t.string :qval
-      t.integer :number_of_lines
-      t.timestamp
+      t.integer :number_of_lines, limit: 8
+      t.timestamp :timestamp
     end
     [ :genome, :agClass, :clClass, :qval, :number_of_lines ].each do |column|
       add_index :bedsizes, column
