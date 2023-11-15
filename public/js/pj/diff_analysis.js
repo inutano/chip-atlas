57e02b9708e9e4f8ba0ef945bc0c8ea19c215165
Tazro Inutano Ohta <inutano@gmail.com>
2023-11-15 12:46:46 +0900

    fetch examples onload

--- public/js/pj/diff_analysis.js
+++ public/js/pj/diff_analysis.js
@@ -1,13 +1,22 @@
 window.onload = async () => {
+  // load analysis examples
+  const examples = await loadExamples();
+  console.log(examples);
   // Initialize
   putDefaultTitles();
   submitDMR();
   // Events
-  putExampleData();
-  switchDataType();
+  putExampleData(examples);
+  switchDataType(examples);
   setGenomePanel();
   estimateTimeOnEdit();
   alertHelpMessage();
+};
+
+const loadExamples = async () => {
+  const response = await fetch("/diff-analysis.examples.json");
+  const examples = await response.json();
+  return examples;
 };
 
 // UI Building
@@ -25,10 +34,10 @@ const putDefaultTitles = () => {
 };
 
 // Change to empty textarea
-const switchDataType = () => {
+const switchDataType = (examples) => {
   $("input.diffOrDMR").change(function () {
     eraseTextarea();
-    removeExampleIfCEBisulfite();
+    removeExampleIfCEBisulfite(examples);
   });
 };
 
@@ -39,7 +48,7 @@ const eraseTextarea = () => {
   $("a#" + genome + "-estimated-run-time").html("-");
 };
 
-const removeExampleIfCEBisulfite = () => {
+const removeExampleIfCEBisulfite = (examples) => {
   const genome = genomeSelected();
   if (genome.startsWith("ce")) {
     // Remove anker tag if DMR is checked
@@ -76,136 +85,31 @@ const removeExampleIfCEBisulfite = () => {
           'dataSetB" name="dataSetB">Try with example</a>',
       );
       // Enable example data insertion
-      putExampleData();
+      putExampleData(examples);
     }
   }
 };
 
 // Example data
-const putExampleData = () => {
+const putExampleData = (examples) => {
   $("a.dataExample").on("click", function (event) {
     event.preventDefault();
     event.stopPropagation();
     const genome = genomeSelected();
     const expType = $('input[name="' + genome + 'DiffOrDMR"]:checked').val();
     let set = $(this).attr("name");
-    let example = diffExampleData[genome][expType][set].split(",").join("\n");
+    const species = genome.replace(/\d+$/, "");
+    let examplesString = examples[species][expType][set].join("\n");
     switch (set) {
       case "dataSetA":
-        $("textarea#" + genome + "DataSetA").val(example);
+        $("textarea#" + genome + "DataSetA").val(examplesString);
         break;
       case "dataSetB":
-        $("textarea#" + genome + "DataSetB").val(example);
+        $("textarea#" + genome + "DataSetB").val(examplesString);
         break;
     }
     calculateEstimatedTime();
   });
-};
-
-const diffExampleData = {
-  hg38: {
-    diff: {
-      dataSetA: "SRX3734456,SRX3734457,SRX3734458",
-      dataSetB: "SRX3734459,SRX3734460,SRX3734461",
-    },
-    DMR: {
-      dataSetA: "SRX10768416,SRX10768417,SRX10768418,SRX10768419,SRX10768420",
-      dataSetB: "SRX10768322,SRX10768323,SRX10768324,SRX10768325,SRX10768326",
-    },
-  },
-  hg19: {
-    diff: {
-      dataSetA: "SRX3734456,SRX3734457,SRX3734458",
-      dataSetB: "SRX3734459,SRX3734460,SRX3734461",
-    },
-    DMR: {
-      dataSetA: "SRX10768416,SRX10768417,SRX10768418,SRX10768419,SRX10768420",
-      dataSetB: "SRX10768322,SRX10768323,SRX10768324,SRX10768325,SRX10768326",
-    },
-  },
-  mm10: {
-    diff: {
-      dataSetA: "SRX7860393,SRX7860394,SRX7860395,SRX7860396,SRX7860397",
-      dataSetB:
-        "SRX7860398,SRX7860399,SRX7860400,SRX7860401,SRX7860402,SRX7860403",
-    },
-    DMR: {
-      dataSetA: "SRX2627050,SRX2627051,SRX2627052,SRX2627053",
-      dataSetB: "SRX2627054,SRX2627055,SRX2627056,SRX2627057",
-    },
-  },
-  mm9: {
-    diff: {
-      dataSetA: "SRX7860393,SRX7860394,SRX7860395,SRX7860396,SRX7860397",
-      dataSetB:
-        "SRX7860398,SRX7860399,SRX7860400,SRX7860401,SRX7860402,SRX7860403",
-    },
-    DMR: {
-      dataSetA: "SRX2627050,SRX2627051,SRX2627052,SRX2627053",
-      dataSetB: "SRX2627054,SRX2627055,SRX2627056,SRX2627057",
-    },
-  },
-  rn6: {
-    diff: {
-      dataSetA: "SRX10157980,SRX10157981,SRX10157982,SRX10157983,SRX10157984",
-      dataSetB:
-        "SRX10158000,SRX10158001,SRX10158002,SRX10158003,SRX10158004,SRX10158005",
-    },
-    DMR: {
-      dataSetA: "SRX10920632,SRX10920633,SRX10920634,SRX10920635",
-      dataSetB: "SRX10920622,SRX10920623,SRX10920624,SRX10920625",
-    },
-  },
-  dm6: {
-    diff: {
-      dataSetA: "SRX7277007,SRX7277008,SRX7277009",
-      dataSetB: "SRX7277010,SRX7277011,SRX7277012",
-    },
-    DMR: {
-      dataSetA: "SRX1552727,SRX1552728,SRX1552729,SRX1552730",
-      dataSetB: "SRX1552723,SRX1552724,SRX1552725,SRX1552726",
-    },
-  },
-  dm3: {
-    diff: {
-      dataSetA: "SRX7277007,SRX7277008,SRX7277009",
-      dataSetB: "SRX7277010,SRX7277011,SRX7277012",
-    },
-    DMR: {
-      dataSetA: "SRX1552727,SRX1552728,SRX1552729,SRX1552730",
-      dataSetB: "SRX1552723,SRX1552724,SRX1552725,SRX1552726",
-    },
-  },
-  ce11: {
-    diff: {
-      dataSetA: "SRX3029121,SRX3029122,SRX3029123",
-      dataSetB: "SRX3029124,SRX3029125,SRX3029126",
-    },
-    DMR: {
-      dataSetA: "",
-      dataSetB: "",
-    },
-  },
-  ce10: {
-    diff: {
-      dataSetA: "SRX3029121,SRX3029122,SRX3029123",
-      dataSetB: "SRX3029124,SRX3029125,SRX3029126",
-    },
-    DMR: {
-      dataSetA: "",
-      dataSetB: "",
-    },
-  },
-  sacCer3: {
-    diff: {
-      dataSetA: "SRX4555040,SRX4555064,SRX4555025",
-      dataSetB: "SRX4555051,SRX4555039,SRX4555013",
-    },
-    DMR: {
-      dataSetA: "SRX957102,SRX957112,SRX957113",
-      dataSetB: "SRX957114,SRX957115,SRX957117",
-    },
-  },
 };
 
 // Diff Analysis post functions
@@ -231,16 +135,7 @@ const submitDMR = async () => {
 
 const retrievePostData = () => {
   const genome = genomeSelected();
-  const expTypeVal = $('input[name="' + genome + 'DiffOrDMR"]:checked').val();
-  let expTypeClass;
-  switch (expTypeVal) {
-    case "diff":
-      expTypeClass = "diffbind";
-      break;
-    case "DMR":
-      expTypeClass = "dmr";
-      break;
-  }
+  const expTypeClass = $('input[name="' + genome + 'DiffOrDMR"]:checked').val();
   const data = {
     // address: '',
     // qsubOptions: '-N test',
