bcf694768322a46e6153f386e433a78dbeda437a
Tazro Inutano Ohta <inutano@gmail.com>
2024-12-04 16:11:00 +0900

    wip

---
+++ script/enrichment-analysis/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# bedA="${1}"
+# bedB="${2}"
+# typeA="${3}"
+# typeB="${4}"
+# descriptionA="${5}"
+# descriptionB="${6}"
+# title="${7}"
+# permTime="${8}"
+# distanceDown="${9}"
+# distanceUp="${10}"
+# genome="${11}"
+# antigenClass="${12}"
+# cellClass="${13}"
+# threshold="${14}"
+# job_id="${15}"
+
+. ./enrichment-analysis.sh \
+"./geneList_A.txt" \
+"./geneList_B.txt" \
+"gene" \
+"gene" \
+"dataset A" \
+"dataset B" \
+"My project" \
+1 \
+5000 \
+5000 \
+"mm9" \
+"TFs and others" \
+"All cell types" \
+100 \
+"jobid-hogehoge"
