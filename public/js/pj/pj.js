c79182ef5c56f062299386515f270908bcc561c4
Tazro Inutano Ohta <inutano@gmail.com>
2025-09-17 02:03:37 +0900

    fix navbar 4

--- public/js/pj/pj.js
+++ public/js/pj/pj.js
@@ -55,78 +55,33 @@ function initResponsiveNavbar() {
       console.log("Breakpoint: < 1346px for hamburger menu");
       console.log("Should show hamburger:", $(window).width() < 1346);
       console.log("Hamburger visible:", $(".navbar-toggle").is(":visible"));
-      console.log(
-        "Navbar collapsed:",
-        $(".navbar-collapse").hasClass("collapse"),
-      );
-      console.log("Bootstrap loaded:", typeof $.fn.collapse !== "undefined");
+      console.log("Collapse state:", $(".navbar-collapse").hasClass("in"));
     }
   }
 
-  // Ensure hamburger button is always visible when needed
-  function checkHamburgerVisibility() {
-    var windowWidth = $(window).width();
-    var $toggle = $(".navbar-toggle");
-    var $collapse = $(".navbar-collapse");
-    var $nav = $(".navbar-nav");
-
-    if (windowWidth < 1346) {
-      $toggle.show().removeClass("hidden");
-      $collapse
-        .addClass("collapse")
-        .removeClass("in")
-        .attr("aria-expanded", "false");
-      $toggle.addClass("collapsed").attr("aria-expanded", "false");
-      // Hide regular nav items, they'll show in collapsed menu
-      $nav.hide();
-    } else {
-      $toggle.hide();
-      $collapse.removeClass("collapse in").removeAttr("style");
-      // Show regular nav items
-      $nav.show();
-    }
-  }
-
-  // Initial check and debug
-  checkHamburgerVisibility();
-  debugNavbar();
-
-  // Ensure navbar collapses properly on small screens
-  $(".navbar-toggle").on("click", function () {
-    var target = $(this).attr("data-target");
-    var $target = $(target);
-    var $nav = $target.find(".navbar-nav");
+  // Simple hamburger toggle - CSS handles the hiding/showing
+  $(".navbar-toggle").on("click", function (e) {
+    e.preventDefault();
+    var $target = $("#navbar");
+    var $button = $(this);
 
     if ($target.hasClass("in")) {
-      $target.removeClass("in").attr("aria-expanded", "false");
-      $(this).addClass("collapsed").attr("aria-expanded", "false");
-      $nav.hide();
+      // Close menu
+      $target.removeClass("in");
+      $button.addClass("collapsed").attr("aria-expanded", "false");
     } else {
-      $target.addClass("in").attr("aria-expanded", "true");
-      $(this).removeClass("collapsed").attr("aria-expanded", "true");
-      $nav.show();
+      // Open menu
+      $target.addClass("in");
+      $button.removeClass("collapsed").attr("aria-expanded", "true");
     }
     debugNavbar();
   });
 
-  // Close navbar when clicking on menu items (on mobile)
+  // Close navbar when clicking on menu items
   $(".navbar-nav li a").on("click", function () {
     if ($(window).width() < 1346) {
-      $(".navbar-collapse").collapse("hide");
-    }
-  });
-
-  // Handle window resize to ensure proper navbar behavior
-  $(window).resize(function () {
-    checkHamburgerVisibility();
-    debugNavbar();
-  });
-
-  // Improve search form behavior on mobile
-  $("#jumpToExperiment").on("focus", function () {
-    if ($(window).width() < 1346) {
-      // Scroll to top to ensure search field is visible
-      $("html, body").animate({ scrollTop: 0 }, 300);
+      $("#navbar").removeClass("in");
+      $(".navbar-toggle").addClass("collapsed").attr("aria-expanded", "false");
     }
   });
 
@@ -137,26 +92,22 @@ function initResponsiveNavbar() {
     if (expid.trim()) {
       window.open("/view?id=" + expid);
     }
-    // Close navbar on mobile after search
+    // Close navbar after search
     if ($(window).width() < 1346) {
-      $(".navbar-collapse").collapse("hide");
+      $("#navbar").removeClass("in");
+      $(".navbar-toggle").addClass("collapsed").attr("aria-expanded", "false");
     }
   });
 
-  // Additional click handler to ensure proper toggling
-  $(".navbar-toggle").on("click", function (e) {
-    // Let the first click handler run first, then this one
-    var self = this;
-    setTimeout(function () {
-      var $target = $($(self).attr("data-target"));
-      var $nav = $target.find(".navbar-nav");
-
-      // Ensure nav visibility matches collapse state
-      if ($target.hasClass("in")) {
-        $nav.show();
-      } else {
-        $nav.hide();
-      }
-    }, 50);
+  // Reset navbar state on window resize
+  $(window).resize(function () {
+    if ($(window).width() >= 1346) {
+      $("#navbar").removeClass("in");
+      $(".navbar-toggle").addClass("collapsed").attr("aria-expanded", "false");
+    }
+    debugNavbar();
   });
+
+  // Initial debug
+  debugNavbar();
 }
