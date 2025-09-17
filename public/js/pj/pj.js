af87abd9c735a969c2f490cd001cae17e7aa12de
Tazro Inutano Ohta <inutano@gmail.com>
2025-09-17 22:38:06 +0900

    forget the hamburger

--- public/js/pj/pj.js
+++ public/js/pj/pj.js
@@ -47,44 +47,6 @@ function tabControl(genome) {
 }
 
 function initResponsiveNavbar() {
-  // Debug function to log navbar state
-  function debugNavbar() {
-    if (window.location.search.includes("debug=navbar")) {
-      console.log("Navbar Debug Info:");
-      console.log("Window width:", $(window).width());
-      console.log("Breakpoint: < 1376px for hamburger menu");
-      console.log("Should show hamburger:", $(window).width() < 1376);
-      console.log("Hamburger visible:", $(".navbar-toggle").is(":visible"));
-      console.log("Collapse state:", $(".navbar-collapse").hasClass("in"));
-    }
-  }
-
-  // Simple hamburger toggle - CSS handles the hiding/showing
-  $(".navbar-toggle").on("click", function (e) {
-    e.preventDefault();
-    var $target = $("#navbar");
-    var $button = $(this);
-
-    if ($target.hasClass("in")) {
-      // Close menu
-      $target.removeClass("in");
-      $button.addClass("collapsed").attr("aria-expanded", "false");
-    } else {
-      // Open menu
-      $target.addClass("in");
-      $button.removeClass("collapsed").attr("aria-expanded", "true");
-    }
-    debugNavbar();
-  });
-
-  // Close navbar when clicking on menu items
-  $(".navbar-nav li a").on("click", function () {
-    if ($(window).width() < 1376) {
-      $("#navbar").removeClass("in");
-      $(".navbar-toggle").addClass("collapsed").attr("aria-expanded", "false");
-    }
-  });
-
   // Handle form submission in navbar
   $(".navbar-form").on("submit", function (e) {
     e.preventDefault();
@@ -92,22 +54,5 @@ function initResponsiveNavbar() {
     if (expid.trim()) {
       window.open("/view?id=" + expid);
     }
-    // Close navbar after search
-    if ($(window).width() < 1376) {
-      $("#navbar").removeClass("in");
-      $(".navbar-toggle").addClass("collapsed").attr("aria-expanded", "false");
-    }
   });
-
-  // Reset navbar state on window resize
-  $(window).resize(function () {
-    if ($(window).width() >= 1376) {
-      $("#navbar").removeClass("in");
-      $(".navbar-toggle").addClass("collapsed").attr("aria-expanded", "false");
-    }
-    debugNavbar();
-  });
-
-  // Initial debug
-  debugNavbar();
 }
